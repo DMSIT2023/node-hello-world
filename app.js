915957a24f2714481a44655db54ba8e94acd8874
DMSIT2023 <131621844+DMSIT2023@users.noreply.github.com>
2023-04-23 17:05:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ const port = 8080;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello World from IBM Cloud Essentials!');
-})
+  const html = `
+    <div>
+      <h1>Hello World from Colombia AWS!</h1>
+      <img src="https://mauricioserranoit.com.co/wp-content/uploads/2022/01/LANDING-PAGE-1-1.webp" alt="Example Image">
+    </div>
+  `;
+  res.send(html);
+});
 
 app.listen(port, host);
 console.log(`Running on http://${host}:${port}`);
